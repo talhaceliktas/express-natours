3c9d6b5f7ac7012c5cb6486ee82ca2af2d304aa0
talhaceliktas <avrol928@gmail.com>
2025-10-04 15:12:25 +0300

    add dotenv

--- app.ts
+++ app.ts
@@ -9,11 +9,16 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
+
 const app = express();
 const API_PREFIX = "/api/v1/";
 
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
 app.use(express.json());
-app.use(morgan("dev"));
 app.use(express.static(`${__dirname}/public`));
 
 app.use(`${API_PREFIX}tours`, tourRouter);
--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const port = 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
