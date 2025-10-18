dcebfa5c9e6042a654bb1c24f5d46b6a9a09d047
talhaceliktas <avrol928@gmail.com>
2025-10-18 11:48:19 +0300

    add document middleware

--- models/tourModel.ts
+++ models/tourModel.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import slugify from "slugify";
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, "A tour must have a duration"],
@@ -59,6 +61,23 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
+});
+
+// Document Middleware: runs before .save() and .create
+tourSchema.pre("save", function (next) {
+  // @ts-ignore
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre("save", function (next) {
+  console.log("Will save document...");
+  next();
+});
+
+tourSchema.post("save", function (doc, next) {
+  console.log(doc);
+  next();
 });
 
 export default mongoose.model("Tour", tourSchema);
