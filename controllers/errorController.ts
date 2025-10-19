c42970a5c7528b33bcc834095ff801d0fcd19ea7
talhaceliktas <avrol928@gmail.com>
2025-10-20 00:37:03 +0300

    add more error handlers

--- controllers/errorController.ts
+++ controllers/errorController.ts
@@ -6,6 +6,22 @@ const handleCastErrorDB = (err: any) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err: any) => {
+  const duplicateName = err.errmsg.match(/name: "(.+)"/)[1];
+
+  const message = `Duplicate field valuie: "${duplicateName}". Please use another value!`;
+
+  return new AppError(message, 400);
+};
+
+const handleValidationError = (err: any) => {
+  const errors = Object.values(err.errors).map((el: any) => el.message);
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err: any, res: Response) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -45,7 +61,8 @@ export default (err: any, req: Request, res: Response, next: NextFunction) => {
     let error = err;
 
     if (error.name === "CastError") error = handleCastErrorDB(error);
-
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError") error = handleValidationError(err);
     sendErrorProd(error, res);
   }
 };
