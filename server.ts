1110519dcbd42c519fee8353669aa875fb8791a8
talhaceliktas <avrol928@gmail.com>
2025-10-20 00:58:06 +0300

    add error handler for uncaughtExceptions

--- server.ts
+++ server.ts
@@ -1,6 +1,12 @@
 import app from "./app.ts";
 import mongoose from "mongoose";
 
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught rejection. Shutting down...");
+  console.log(err);
+  process.exit(1);
+});
+
 const port = process.env.PORT || 8000;
 
 const DB = process.env.DATABASE!.replace(
@@ -17,8 +23,8 @@ const server = app.listen(port, () => {
 });
 
 process.on("unhandledRejection", (err: Error) => {
-  console.log(err.name, err.message);
   console.log("Unhandled rejection. Shutting down...");
+  console.log(err);
   server.close(() => {
     process.exit(1);
   });
