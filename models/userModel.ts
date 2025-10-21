3b5bbff7367285bfd0f8110156ac4c8404effedb
talhaceliktas <avrol928@gmail.com>
2025-10-22 00:29:25 +0300

    add argon2 hash algorithm

--- models/userModel.ts
+++ models/userModel.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import argon2 from "@node-rs/argon2";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,7 +23,29 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, "Please confirm your password"],
+    validate: {
+      // This only works on CREATE and SAVE!!
+      validator: function (val: String) {
+        return val === this.password;
+      },
+      message: "Passwords are not the same",
+      select: false,
+    },
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await argon2.hash(this.password, {
+    memoryCost: 19456,
+    timeCost: 2,
+    parallelism: 1,
+  });
+
+  this.passwordConfirm = undefined as any;
+
+  next();
 });
 
 export default mongoose.model("User", userSchema);
