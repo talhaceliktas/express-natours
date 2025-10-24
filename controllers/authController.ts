089150f89525211f40ae4407f9edb44a6a6707d9
talhaceliktas <avrol928@gmail.com>
2025-10-25 00:23:14 +0300

    add jwt functionality

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,4 +1,5 @@
 import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import User from "../models/userModel.ts";
 import catchAsync from "../utils/catchAsync.ts";
 
@@ -11,8 +12,14 @@ const signUp = catchAsync(
       passwordConfirm: req.body.passwordConfirm,
     });
 
+    // @ts-ignore
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET!, {
+      expiresIn: process.env.JWT_EXPIRES_IN!,
+    });
+
     res.status(201).json({
       status: "success",
+      token,
       data: {
         user: newUser,
       },
